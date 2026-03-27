78147807813fccc373d37413a36b4c75ee780cd3
raven-panda <ambroise.maire7@gmail.com>
2026-03-27 17:37:09 +0100

    feat(login form): created login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { UserService } from '../../core/service/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Login } from '../../core/models/Login';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MaterialModule } from '../../shared/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './login.component.html',
+  standalone: true,
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private destroyRef: DestroyRef) {}
 
+  loginForm: FormGroup = new FormGroup({});
+  submitted: boolean = false;
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group(
+      {
+        login: ['', Validators.required],
+        password: ['', Validators.required]
+      },
+    );
+  }
+
+  get form() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const registerUser: Login = {
+      login: this.loginForm.get('login')?.value,
+      password: this.loginForm.get('password')?.value
+    };
+    this.userService.login(registerUser)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(
+      (data) => {
+        console.log({data});
+        alert('SUCCESS!! :-) Jwt : ');
+        // TODO : router l'utilisateur vers la page de login
+      },
+    );
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.loginForm.reset();
+  }
 }
+
