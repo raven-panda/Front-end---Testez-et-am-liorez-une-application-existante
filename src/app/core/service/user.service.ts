9b7b3fd506c58063506239ddd3a7c4d48e0d8ca8
raven-panda <ambroise.maire7@gmail.com>
2026-03-27 17:36:50 +0100

    feat(user service): added login dto and method in user service

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { Register } from '../models/Register';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Login } from '../models/Login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   constructor(private httpClient: HttpClient) { }
+
+  login(user: Login): Observable<Object> {
+    return this.httpClient.post('/api/login', user);
+  }
 
   register(user: Register): Observable<Object> {
     return this.httpClient.post('/api/register', user);
