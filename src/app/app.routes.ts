0e8843e4a96baf96636348c5e792d7a8a1c92c60
raven-panda <ambroise.maire7@gmail.com>
2026-03-27 17:30:28 +0100

    feat(login): added login page to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from './pages/register/register.component';
 import {AppComponent} from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 
 ];
